6dfd7c1fbe5d950512bbb2f087a824a75d1ae44f
German Feskov <g.feskov@yandex.ru>
2026-02-14 21:03:14 +0300

    fix execution cli

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"os"
 
+	"github.com/Go-Yadro-Group-1/Jira-Analyzer/cmd/cli"
+)
+
+func main() {
+	err := cli.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
 }
