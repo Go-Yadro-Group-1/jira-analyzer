3b0832a2f7b6d75a81977e386fefbfa8b7538371
German Feskov <g.feskov@yandex.ru>
2026-02-19 22:47:09 +0300

    add issue model

--- internal/repository/postgres/raw_model.go
+++ internal/repository/postgres/raw_model.go
@@ -1,5 +1,7 @@
 package postgres
 
+import "time"
+
 type Project struct {
 	ID    int
 	Title string
@@ -8,4 +10,21 @@ type Project struct {
 type Author struct {
 	ID   int
 	Name string
+}
+
+type Issue struct {
+	ID          int
+	ProjectID   int
+	AuthorID    int
+	AssigneeID  int
+	Key         string
+	Summary     string
+	Description string
+	Type        string
+	Priority    string
+	Status      string
+	CreatedAt   time.Time
+	ClosedAt    time.Time
+	UpdatedAt   time.Time
+	TimeSpent   int
 }
