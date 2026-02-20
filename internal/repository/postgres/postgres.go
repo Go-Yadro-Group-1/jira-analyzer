b5921a3c249584bebf52c553ec2e13e059ff2257
German Feskov <g.feskov@yandex.ru>
2026-02-20 14:53:26 +0300

    delete other methods

--- internal/repository/postgres/postgres.go
+++ internal/repository/postgres/postgres.go
@@ -11,12 +11,3 @@ func New(db *sql.DB) *Postgres {
 		db: db,
 	}
 }
-
-// Get Count of Issues in Project by ID
-func GetIssueCountInProject(projectID int) int
-
-// Get Count of Open Issues in Project by ID
-func GetOpenIssueCountInProject(projectID int) int
-
-// Get Count of Closed Issues in Project by ID
-func GetClosedIssueCountInProject(projectID int) int
