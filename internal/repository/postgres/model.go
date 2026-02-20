5e3d9c0c1cc9bef9eb44cb5d10e2dab3362ecbda
German Feskov <g.feskov@yandex.ru>
2026-02-20 14:55:14 +0300

    fix linter warns

--- internal/repository/postgres/model.go
+++ internal/repository/postgres/model.go
@@ -2,7 +2,7 @@ package postgres
 
 import "time"
 
-// get raw statistics for a Jira project ОФ2
+// ProjectStats get raw statistics for a Jira project ОФ2.
 type ProjectStats struct {
 	ProjectID        int
 	Total            int
@@ -15,34 +15,34 @@ type ProjectStats struct {
 	AvgDailyLastWeek float64
 }
 
-// information about the duration of closed issues ОФ2.1
+// IssueDuration information about the duration of closed issues ОФ2.1.
 type IssueDuration struct {
 	IssueID  int
 	Duration time.Duration
 }
 
-// time a closed issue spent in each status ОФ2.2
+// IssueByStatusDuration time a closed issue spent in each status ОФ2.2.
 type IssueByStatusDuration struct {
 	IssueID  int
 	Status   string
 	Duration time.Duration
 }
 
-// number of created and closed issues per day ОФ2.3
+// DailyActivity number of created and closed issues per day ОФ2.3.
 type DailyActivity struct {
 	Date       time.Time
 	Creation   int
 	Completion int
 }
 
-// logged time spent on a closed issue ОФ2.4
+// IssueTimeSpent logged time spent on a closed issue ОФ2.4.
 type IssueTimeSpent struct {
 	IssueID   int
 	TimeSpent time.Duration
 }
 
-// number of issues per priority level ОФ2.5
-type Priority struct {
+// PriorityStats number of issues per priority level ОФ2.5.
+type PriorityStats struct {
 	Priority string
 	Count    int
 }
