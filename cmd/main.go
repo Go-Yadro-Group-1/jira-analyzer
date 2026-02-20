95f5cb8dbdcccdd12ead39243ae33c665e0633b6
German Feskov <g.feskov@yandex.ru>
2026-02-21 00:04:38 +0300

    feat(cli): add serve and version commands with PostgreSQL support

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,26 @@
 package main
 
 import (
-	"github.com/Go-Yadro-Group-1/Jira-Analyzer/cmd/internal/cli"
+	"os"
+
+	"github.com/Go-Yadro-Group-1/Jira-Analyzer/cmd/internal/cli/server"
+	"github.com/Go-Yadro-Group-1/Jira-Analyzer/cmd/internal/cli/version"
+	"github.com/spf13/cobra"
 )
 
 func main() {
-	err := cli.Execute()
+	//nolint:exhaustruct
+	rootCmd := &cobra.Command{
+		Use:   "jira-analyzer",
+		Short: "Jira Analyzer",
+		Long:  "Jira Analyzer is a tool for analyzing Jira issues.",
+	}
+
+	rootCmd.AddCommand(server.NewCommand())
+	rootCmd.AddCommand(version.NewCommand())
+
+	err := rootCmd.Execute()
 	if err != nil {
-		panic(err)
+		os.Exit(1)
 	}
 }
