d4ae87a974d09fc3fa4d6b881386a27fa6d65cf6
German Feskov <g.feskov@yandex.ru>
2026-02-20 15:10:20 +0300

    fix linter warns

--- internal/repository/postgres/postgres.go
+++ internal/repository/postgres/postgres.go
@@ -21,26 +21,41 @@ func (p *Postgres) GetStatsByProject(ctx context.Context, projectID int) (Projec
 }
 
 // nolint: revive, godoclint
-func (p *Postgres) GetIssuesDurationByProject(ctx context.Context, projectID int) ([]IssueDuration, error) {
+func (p *Postgres) GetIssuesDurationByProject(
+	ctx context.Context,
+	projectID int,
+) ([]IssueDuration, error) {
 	return []IssueDuration{}, nil
 }
 
 // nolint: revive, godoclint
-func (p *Postgres) GetIssuesByStatusDurationByProject(ctx context.Context, projectID int) ([]IssueByStatusDuration, error) {
+func (p *Postgres) GetIssuesByStatusDurationByProject(
+	ctx context.Context,
+	projectID int,
+) ([]IssueByStatusDuration, error) {
 	return []IssueByStatusDuration{}, nil
 }
 
 // nolint: revive, godoclint
-func (p *Postgres) GetDailyActivityByProject(ctx context.Context, projectID int) ([]DailyActivity, error) {
+func (p *Postgres) GetDailyActivityByProject(
+	ctx context.Context,
+	projectID int,
+) ([]DailyActivity, error) {
 	return []DailyActivity{}, nil
 }
 
 // nolint: revive, godoclint
-func (p *Postgres) GetIssuesTimeSpentByProject(ctx context.Context, projectID int) ([]IssueTimeSpent, error) {
+func (p *Postgres) GetIssuesTimeSpentByProject(
+	ctx context.Context,
+	projectID int,
+) ([]IssueTimeSpent, error) {
 	return []IssueTimeSpent{}, nil
 }
 
 // nolint: revive, godoclint
-func (p *Postgres) GetPriorityStatsByProject(ctx context.Context, projectID int) ([]PriorityStats, error) {
+func (p *Postgres) GetPriorityStatsByProject(
+	ctx context.Context,
+	projectID int,
+) ([]PriorityStats, error) {
 	return []PriorityStats{}, nil
 }
