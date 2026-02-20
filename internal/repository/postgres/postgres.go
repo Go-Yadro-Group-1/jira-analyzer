1a0f7c84637b55cf346ffd24eda207fdd2e50d80
German Feskov <g.feskov@yandex.ru>
2026-02-20 15:09:49 +0300

    init some methods for postgres

--- internal/repository/postgres/postgres.go
+++ internal/repository/postgres/postgres.go
@@ -16,6 +16,31 @@ func New(db *sql.DB) *Postgres {
 }
 
 // nolint: revive, godoclint
-func GetStatsByProject(ctx context.Context, projectID int) (ProjectStats, error) {
+func (p *Postgres) GetStatsByProject(ctx context.Context, projectID int) (ProjectStats, error) {
 	return ProjectStats{}, nil
+}
+
+// nolint: revive, godoclint
+func (p *Postgres) GetIssuesDurationByProject(ctx context.Context, projectID int) ([]IssueDuration, error) {
+	return []IssueDuration{}, nil
+}
+
+// nolint: revive, godoclint
+func (p *Postgres) GetIssuesByStatusDurationByProject(ctx context.Context, projectID int) ([]IssueByStatusDuration, error) {
+	return []IssueByStatusDuration{}, nil
+}
+
+// nolint: revive, godoclint
+func (p *Postgres) GetDailyActivityByProject(ctx context.Context, projectID int) ([]DailyActivity, error) {
+	return []DailyActivity{}, nil
+}
+
+// nolint: revive, godoclint
+func (p *Postgres) GetIssuesTimeSpentByProject(ctx context.Context, projectID int) ([]IssueTimeSpent, error) {
+	return []IssueTimeSpent{}, nil
+}
+
+// nolint: revive, godoclint
+func (p *Postgres) GetPriorityStatsByProject(ctx context.Context, projectID int) ([]PriorityStats, error) {
+	return []PriorityStats{}, nil
 }
