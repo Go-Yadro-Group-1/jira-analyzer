7918bcef3a3eda163bc4e4f6b52cb5164cb2fffd
German Feskov <g.feskov@yandex.ru>
2026-02-20 15:04:58 +0300

    add getstatsbyproject func

--- internal/repository/postgres/postgres.go
+++ internal/repository/postgres/postgres.go
@@ -1,6 +1,9 @@
 package postgres
 
-import "database/sql"
+import (
+	"context"
+	"database/sql"
+)
 
 type Postgres struct {
 	db *sql.DB
@@ -10,4 +13,9 @@ func New(db *sql.DB) *Postgres {
 	return &Postgres{
 		db: db,
 	}
+}
+
+// nolint: revive, godoclint
+func GetStatsByProject(ctx context.Context, projectID int) (ProjectStats, error) {
+	return ProjectStats{}, nil
 }
