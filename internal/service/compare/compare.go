0dd6e58240eb7f0135d1fee50744b0a801b71662
German Feskov <g.feskov@yandex.ru>
2026-02-20 21:51:33 +0300

    delete internal folder

--- internal/service/compare/compare.go
+++
@@ -1,8 +0,0 @@
-// Package compare provides functionality for comparing Jira issues.
-package compare
-
-type Compare struct{}
-
-func New() *Compare {
-	return &Compare{}
-}
--- internal/service/dashboard/dashboard.go
+++
@@ -1,8 +0,0 @@
-// Package dashboard provides functionality for managing Jira dashboards.
-package dashboard
-
-type Dashboard struct{}
-
-func New() *Dashboard {
-	return &Dashboard{}
-}
