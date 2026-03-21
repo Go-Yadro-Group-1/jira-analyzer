46803d4c5775ab2a8be8e7ceb67408bb1ebf5922
German <123095564+6ermvH@users.noreply.github.com>
2026-03-21 20:32:18 +0300

    Revert "refactor(repository): Export CacheRepository struct and initialize mutex"
    This reverts commit 98618556a58e3457cc76b28749e4db6c9352c6a4.

--- internal/repository/memory/cache.go
+++ internal/repository/memory/cache.go
@@ -8,25 +8,20 @@ import (
 	"github.com/Go-Yadro-Group-1/Jira-Analyzer/internal/repository"
 )
 
-type CacheRepository struct {
+type cacheRepository struct {
 	mu        sync.RWMutex
 	data      map[int]map[repository.DataType][]byte
 	updatedAt map[int]time.Time
 }
 
-func NewCacheRepository() *CacheRepository {
-	return &CacheRepository{
-		mu:        sync.RWMutex{},
+func NewCacheRepository() repository.CacheRepository {
+	return &cacheRepository{
 		data:      make(map[int]map[repository.DataType][]byte),
 		updatedAt: make(map[int]time.Time),
 	}
 }
 
-func (c *CacheRepository) Get(
-	_ context.Context,
-	projectID int,
-	dataType repository.DataType,
-) ([]byte, error) {
+func (c *cacheRepository) Get(_ context.Context, projectID int, dataType repository.DataType) ([]byte, error) {
 	c.mu.RLock()
 	defer c.mu.RUnlock()
 
@@ -35,50 +30,40 @@ func (c *CacheRepository) Get(
 			return value, nil
 		}
 	}
-
 	return nil, nil
 }
 
-func (c *CacheRepository) Set(
-	_ context.Context,
-	projectID int,
-	dataType repository.DataType,
-	value []byte,
-) error {
+func (c *cacheRepository) Set(_ context.Context, projectID int, dataType repository.DataType, value []byte) error {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 
 	if _, ok := c.data[projectID]; !ok {
 		c.data[projectID] = make(map[repository.DataType][]byte)
 	}
-
 	c.data[projectID][dataType] = value
-
 	return nil
 }
 
-func (c *CacheRepository) Invalidate(_ context.Context, projectID int) error {
+func (c *cacheRepository) Invalidate(_ context.Context, projectID int) error {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 
 	delete(c.data, projectID)
 	delete(c.updatedAt, projectID)
-
 	return nil
 }
 
-func (c *CacheRepository) GetLastUpdated(_ context.Context, projectID int) (time.Time, error) {
+func (c *cacheRepository) GetLastUpdated(_ context.Context, projectID int) (time.Time, error) {
 	c.mu.RLock()
 	defer c.mu.RUnlock()
 
 	return c.updatedAt[projectID], nil
 }
 
-func (c *CacheRepository) SetLastUpdated(_ context.Context, projectID int, t time.Time) error {
+func (c *cacheRepository) SetLastUpdated(_ context.Context, projectID int, t time.Time) error {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 
 	c.updatedAt[projectID] = t
-
 	return nil
 }
